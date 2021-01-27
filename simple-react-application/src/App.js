8abf0a1dfd762df326ff160cc285988c6a88eeed
Adam Gong <adam.gong.ca@gmail.com>
2021-01-27 17:12:14 -0500

    updated html.

--- simple-react-application/src/App.js
+++ simple-react-application/src/App.js
@@ -9,7 +9,7 @@ function App() {
           This is a React/Javascript exercise<br />
           Here is the front end UI / Full stack Developer's challenge. <br />
           You have about 1 - 2 hours to complete. Once when they are done, we can review the GitHub code, before scheduling interview. <br />
-          I really don't know whose idea, how do you know my skill level through this tiny app.
+          Thank you guys for giving me an interview chance!
         </p>
       </header>
       <div className="App-container">
